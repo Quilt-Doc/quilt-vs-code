ebbfff4282db059bcba12b0aa95b5fad553e6e34
fsanal <fsanal@sas.upenn.edu>
2021-05-20 14:35:21 -0700

    [QD-416] Blame Annotation Tuning/Testing Halted

--- storage.js
+++ storage.js
@@ -14,3 +14,56 @@
 "webpack-cli": "^3.3.12",
 "webpack-dev-server": "^3.11.0",
 */
+
+/*
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+    entry: "./app/index.js",
+    mode: "development",
+    devtool: "eval-source-map",
+    devServer: {
+        contentBase: "./dist",
+        hot: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|ts)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                use: [
+                    {
+                        loader: "url-loader?limit=100000",
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebPackPlugin({
+            template: "./app/index.html",
+            filename: "./index.html",
+        }),
+        new webpack.ProvidePlugin({
+            React: "react",
+            ReactDOM: "react-dom",
+        }),
+    ],
+};*/
